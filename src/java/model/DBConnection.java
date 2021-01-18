c80faa31fbfb9b594acc579f63df1fb5c20697a9
Raul Ginj <ggra99@gmail.com>
2021-01-18 21:45:35 +0200

    Fixed minor error

--- src/java/model/DBConnection.java
+++ src/java/model/DBConnection.java
@@ -2,22 +2,17 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet; // Removed unused imports
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Time;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Arrays;
-import java.util.Iterator;
-//import java.util.Date;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.http.HttpSession;
+
 import model.User;
 import model.BookingSlot;
 
